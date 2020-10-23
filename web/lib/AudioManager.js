9d41af321c8cb32d0f67f6df2392c256b2ec0f6b
LeiWang <levidcd@outlook.com>
2020-10-23 18:35:18 +0800

    配置jest

---
+++ web/lib/AudioManager.js
@@ -0,0 +1,14 @@
+// interface AudioElement {
+//   dom: Element;
+// }
+// interface AudioEnity {}
+
+class AudioManager {
+  constructor(){
+    //  let audioPlayer = new Audio()
+  }
+
+}
+
+
+export default AudioManager

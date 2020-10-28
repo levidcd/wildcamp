233a6da5d068ec2f0ea45738b0e3cdd92c63a941
LeiWang <levidcd@outlook.com>
2020-10-28 10:08:47 +0800

    调优AudioManage

--- web/components/AudioPlayer.tsx
+++ web/components/AudioPlayer.tsx
@@ -2,28 +2,45 @@ import React, { useState } from "react";
 import { PlayCircleOutlined, PauseCircleOutlined } from "@ant-design/icons";
 import styles from "./AudioPlayer.module.scss";
 
+import AudioManager from "../lib/AudioManager";
+
+const audioManager = new AudioManager("music");
+
+console.log(audioManager);
+
 const AudioPlayer = () => {
   // function rbf() {
   //   let audio: any = document.getElementById("music");
   //   audio.currentTime = 0;
   // }
 
+  // function bf() {
+  //   var audio: any = document.getElementById("music");
+  //   if (audio !== null) {
+  //     //检测播放是否已暂停.audio.paused 在播放器播放时返回false.
+  //     if (audio.paused) {
+  //       audio.play(); //audio.play();// 这个就是播放
+  //     } else {
+  //       audio.pause(); // 这个就是暂停
+  //     }
+  //   }
+  // }
   function bf() {
-    var audio: any = document.getElementById("music");
-    if (audio !== null) {
-      //检测播放是否已暂停.audio.paused 在播放器播放时返回false.
-      if (audio.paused) {
-        audio.play(); //audio.play();// 这个就是播放
+    // var audio: any = document.getElementById("music");
+    if (audioManager.isInit) {
+      if (audioManager.paused) {
+        audioManager.play();
       } else {
-        audio.pause(); // 这个就是暂停
+        audioManager.pause(); // 这个就是暂停
       }
+      setIsPlaying(!audioManager.paused);
+    } else {
+      audioManager.init();
     }
   }
 
   const playOrPause = (e: any) => {
-    console.log(e.target);
     bf();
-    setIsPlaying(!isPlaying);
   };
 
   const [isPlaying, setIsPlaying] = useState(false);
@@ -37,9 +54,13 @@ const AudioPlayer = () => {
         }}
       >
         {!isPlaying ? (
-          <PlayCircleOutlined style={{ fontSize: "30px",color:"rgba(176, 14, 37, 0.7)" }} />
+          <PlayCircleOutlined
+            style={{ fontSize: "30px", color: "rgba(176, 14, 37, 0.7)" }}
+          />
         ) : (
-          <PauseCircleOutlined style={{ fontSize: "30px",color:'rgba(176, 14, 37, 0.7)' }} />
+          <PauseCircleOutlined
+            style={{ fontSize: "30px", color: "rgba(176, 14, 37, 0.7)" }}
+          />
         )}
       </div>
 
--- web/lib/AudioManager.ts
+++ web/lib/AudioManager.ts
@@ -4,11 +4,55 @@
 // interface AudioEnity {}
 
 class AudioManager {
-  constructor(){
-    //  let audioPlayer = new Audio()
-  }
+  #ele: string;
+  #audioPlayer: AudioContext | HTMLElement | null;
+  isInit: Boolean;
+  paused: Boolean;
 
+  constructor(ele: string = "") {
+    this.#ele = ele;
+    this.#audioPlayer = null;
+    this.isInit = false;
+    this.paused = true;
+    this.init();
+  }
+  play() {
+    if (!this.#audioPlayer) {
+      this.init();
+    }
+    this.#audioPlayer.play();
+    this.paused = false;
+  }
+  pause() {
+    if (!this.#audioPlayer) {
+      this.init();
+    }
+    this.#audioPlayer.pause();
+    this.paused = true;
+  }
+  fastSeek(time: string | number) {
+    this.#audioPlayer.fastSeek(time);
+  }
+  getStartDate() {
+    return this.#audioPlayer.getStartDate();
+  }
+  init() {
+    if (typeof document !== "undefined") {
+      const audioElement = document.getElementById(this.#ele);
+      this.#audioPlayer = audioElement;
+      this.isInit = true;
+      this.paused = true;
+      this.#audioPlayer.addEventListener("playing", function () {
+        console.log('播放中');
+      });
+      this.#audioPlayer.addEventListener("timeupdate", function () {
+        console.log('播放中');
+      });
+      this.#audioPlayer.addEventListener("pause", function () {
+        console.log('暂停');
+      });
+    }
+  }
 }
 
-
-export default AudioManager
+export default AudioManager;

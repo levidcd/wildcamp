592d39eae26e31f8d202c3f36953316eedd0053b
LeiWang <levidcd@outlook.com>
2020-11-04 17:43:39 +0800

    设置全局前缀

--- servers/apps/web/src/main.ts
+++ servers/apps/web/src/main.ts
@@ -8,6 +8,7 @@ import setupApp from './core/setup/index';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('web/v1')
 
   const options = new DocumentBuilder()
     .setTitle('Cats example')

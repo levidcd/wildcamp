9cd22b34ad657ab4161fe0c1d8576b72879d63e7
LeiWang <levidcd@outlook.com>
2020-11-06 16:48:39 +0800

    脚本修改

--- pullcodingimage.sh
+++ pullcodingimage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker pull docker pull wildcamp-docker.pkg.coding.net/wildcamp/wildcamp/server
-docker pull docker pull wildcamp-docker.pkg.coding.net/wildcamp/wildcamp/web
+docker pull  wildcamp-docker.pkg.coding.net/wildcamp/wildcamp/server
+docker pull  wildcamp-docker.pkg.coding.net/wildcamp/wildcamp/web
 # 运行docker
 docker-compose -f docker-compose.coding.yml stop
 docker-compose -f docker-compose.coding.yml down
